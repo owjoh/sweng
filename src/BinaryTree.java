cd48b7ceb60b282974ae21d2bc811a9a0f2b5a2e
Owen Johnston <32434575+owjoh@users.noreply.github.com>
2019-10-02 13:52:29 +0100

    -Created Tree

--- src/BinaryTree.java
+++ src/BinaryTree.java
@@ -1,6 +1,10 @@
 public class BinaryTree {
     Node root;
 
+    public BinaryTree(Node root) {
+        this.root = root;
+    }
+
     Node lca(int node1, int node2) {
         return findLca(this.root, node1, node2);
     }
@@ -20,5 +24,16 @@ public class BinaryTree {
         findLca(node.right, node1, node2);
 
         return null;
+    }
+
+    public static void main(String args[]) {
+        BinaryTree tree = new BinaryTree(new Node(1));
+        tree.root.left = new Node(2);
+        tree.root.right = new Node(3);
+        tree.root.left.left = new Node(4);
+        tree.root.left.right = new Node(5);
+        tree.root.left.left.left = new Node(6);
+        tree.root.left.left.right = new Node(7);
+
     }
 }
