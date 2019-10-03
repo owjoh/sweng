be1728bc7e8991e7e79ddea6fbf1b29cec8295f5
Owen Johnston <32434575+owjoh@users.noreply.github.com>
2019-10-03 16:22:26 +0100

    -Added more tests

--- src/BinaryTree.java
+++ src/BinaryTree.java
@@ -30,15 +30,4 @@ public class BinaryTree {
         return (left != null) ? left : right;
     }
 
-    public static void main(String args[]) {
-        BinaryTree tree = new BinaryTree(new Node(1));
-        tree.root.left = new Node(2);
-        tree.root.right = new Node(3);
-        tree.root.left.left = new Node(4);
-        tree.root.left.right = new Node(5);
-        tree.root.left.left.left = new Node(6);
-        tree.root.left.left.right = new Node(7);
-        System.out.println(tree.lca(7,5).data);
-
-    }
 }
--- src/BinaryTreeTest.java
+++ src/BinaryTreeTest.java
@@ -4,19 +4,31 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class BinaryTreeTest {
 
-    BinaryTree tree = new BinaryTree(null);
 
     @Test
     void testNullTree() {
-        assertNull(tree.lca(1,2));
+        BinaryTree tree = new BinaryTree(null);
+        assertNull(tree.lca(1, 2));
     }
 
-
-    @org.junit.jupiter.api.Test
-    void lca() {
+    @Test
+    void testOneNodeTree() {
+        Node root = new Node(0);
+        BinaryTree tree = new BinaryTree(root);
+        assertTrue(tree.lca(0, 0) == root);
     }
 
-    @org.junit.jupiter.api.Test
-    void findLca() {
+    @Test
+    void testNodePredecessor() {
+        Node root = new Node(0);
+        BinaryTree tree = new BinaryTree(root);
+        tree.root.left = new Node(2);
+        tree.root.right = new Node(3);
+        tree.root.left.left = new Node(4);
+        tree.root.left.right = new Node(5);
+        tree.root.left.left.left = new Node(6);
+        tree.root.left.left.right = new Node(7);
+        assertTrue(tree.lca(0, 2) == root);
+        assertTrue(tree.lca(6, 5) == tree.root.left);
     }
 }
