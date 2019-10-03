d45225c3a8757ceef30cecc05fff03940f0ff9b8
Owen Johnston <32434575+owjoh@users.noreply.github.com>
2019-10-03 12:15:36 +0100

    -Configured JUnit for testing in IntelliJ -Wrote first few tests

---
+++ src/BinaryTreeTest.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BinaryTreeTest {
+
+    BinaryTree tree = new BinaryTree(null);
+
+    @Test
+    void testNullTree() {
+        assertNull(tree.lca(1,2));
+    }
+
+
+    @org.junit.jupiter.api.Test
+    void lca() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void findLca() {
+    }
+}
