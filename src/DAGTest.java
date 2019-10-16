ac9b40d736ac3b0ade5620b7890a3f96cf1351f3
Owen Johnston <32434575+owjoh@users.noreply.github.com>
2019-10-16 12:32:58 +0100

    Create DAGTest.java
    -Created test file for DAG.
    -Created some empty tests of the cases I want to test once lca algorithm is finished.

---
+++ src/DAGTest.java
@@ -0,0 +1,27 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DAGTest {
+
+    @Test
+    void testNullTree() {
+        
+    }
+
+    @Test
+    void testOneNodeTree() {
+
+    }
+
+    @Test
+    void testLcaOnTheSameNode() {
+
+    }
+
+    @Test
+    void testNodeThatDoesNotExist() {
+
+    }
+
+}
